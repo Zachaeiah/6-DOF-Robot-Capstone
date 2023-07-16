d401e2733fe376fdd577940989e0d9ae99564c0c
Zachariah Loewen <zloewen314@gmail.com>
2023-07-16 16:42:13 -0700

    got motors working

--- Arduino code/Single_motor_driver/Single_motor_driver.ino
+++ Arduino code/Single_motor_driver/Single_motor_driver.ino
@@ -1,7 +1,18 @@
 #include <math.h>
+#include <stdlib.h>
 //---------------------------------------------------------------------------------------------------------------------
+
+enum MOTOR_DIR { FORWARDS,
+                 BACKWARDS };
+
+typedef struct STEP_PLAN {
+  double totalDelay;
+  long* delays;
+} STEP_PLAN;
+
 typedef struct CONTROL_FEATURS {
   const long pulesRev;
+  const double degStep;
   double maxSpeed;
   int direcstion;
 } CONTROL_FEATURS;
@@ -19,35 +30,151 @@ typedef struct MOTOR_CONTROLLER {
   size_t motorNum;
 
 } MOTOR_CONTROLLER;
+
+const int MotorPins_len = 12;
+int MotorPins[MotorPins_len] = { 2, 3, 4, 5, 6, 9, 10, 11, 12, 26, 27, 30 };
+
 //---------------------------------------------------------------------------------------------------------------------
 
-STEPPER_MOTOR Motor1 = { "test3", 6, 9, 2.84, 400, 100, 1 };
+STEPPER_MOTOR bace = { "bace", MotorPins[0], MotorPins[1], 2.84, 400, 1.1, 100. FORWARDS };
+STEPPER_MOTOR shoulder = { "shoulder", MotorPins[2], MotorPins[3], 2.84, 400, 1.1, 100,  FORWARDS };
+STEPPER_MOTOR elbow = { "elbow", MotorPins[4], MotorPins[5], 2.84, 400, 1.1, 100, 1, FORWARDS };
+STEPPER_MOTOR elbowRevolut = { "elbowRevolut", MotorPins[6], MotorPins[7], 2.84, 400, 1.1, 100, FORWARDS };
+STEPPER_MOTOR wrist = { "wrist", MotorPins[8], MotorPins[9], 2.84, 400, 1.1, 100, 1, FORWARDS };
+STEPPER_MOTOR wristRevolut = { "wristRevolut", MotorPins[10], MotorPins[11], 2.84, 400, 1.1, 100, FORWARDS };
+
+STEPPER_MOTOR robotMotors[6] = { bace, shoulder, elbow, elbowRevolut, wrist, wristRevolut };
 
 //---------------------------------------------------------------------------------------------------------------------
 void setup() {
   Serial.begin(9600);
-  pinMode(Motor1.dirPin, OUTPUT);
-  pinMode(Motor1.stepPin, OUTPUT);
+  for (int pin = 0; pin < MotorPins_len; pin++) {
+    pinMode(MotorPins[pin], OUTPUT);
+  }
 
-  singleLinRotate(Motor1, 5000);
+  long Steps[6] = { 1000, 2000, 4000, 8000, 16000, 32000 };
 
+  MultyLinRotate(robotMotors, Steps);
+
+  //singleLinRotate(wristRevolut, 5000);
 }
 
 //---------------------------------------------------------------------------------------------------------------------
 void loop() {
 }
 
-//---------------------------------------------------------------------------------------------------------------------
-void singleLinRotate(STEPPER_MOTOR motor, long steps) {
+void MultyLinRotate(STEPPER_MOTOR Motor[], long Steps[]) {
 
-  long plulseDealay = 0;
-  double degStep = 360 / (double)(motor.control.pulesRev);
-  for (int step = 1; step <= steps; step++) {
-    plulseDealay = ((degStep * 60.0) / (motor.control.maxSpeed * 360)) * pow(10, 6);
-    Serial.println(plulseDealay);
-    singleStep(motor, plulseDealay);
+  long currentDelay;
+  double motorPlans[6] = { 0 };
+  double largestTime = 0;
+  int largestTimeIndex = 0;
+  double oldSpeeds[6] = { 0 };
+
+  double maxSpeed = 0;
+  double degStep = 0;
+  double gostTime = 0;
+
+
+  largestTime = motorPlans[0];
+
+  for (int plan = 0; plan < 6; plan++) {
+
+    maxSpeed = Motor[plan].control.maxSpeed;
+    degStep = Motor[plan].control.degStep;
+
+    for (int step = 0; step < Steps[plan]; step++) {
+      currentDelay = (long)(((degStep * 60.0) / (maxSpeed * 360.0)) * pow(10, 6));
+      motorPlans[plan] += (double)currentDelay / 1000000.0;
+    }
+    if (motorPlans[plan] > largestTime) {
+      largestTime = motorPlans[plan];
+      largestTimeIndex = plan;  // Track the index of the largest time
+    }
+  }
+
+  Serial.print("Goal time is:");
+  Serial.println(largestTime);
+
+  for (int plan = 0; plan < 2; plan++) {
+    if (plan == largestTimeIndex) {
+      continue;
+    }
+    oldSpeeds[plan] = Motor[plan].control.maxSpeed;
+    Motor[plan].control.maxSpeed = ((double)Steps[plan]) / largestTime;
+
+    maxSpeed = Motor[plan].control.maxSpeed;
+    degStep = Motor[plan].control.degStep;
+
+    for (int step = 0; step < Steps[plan]; step++) {
+      currentDelay = (long)(((degStep * 60.0) / (maxSpeed * 360.0)) * pow(10, 6));
+      gostTime += (double)currentDelay / 1000000.0;
+    }
+
+    Serial.print("Motor: ");
+    Serial.println(plan);
+    Serial.print("old time");
+    Serial.println(motorPlans[plan]);
+    Serial.print("New time");
+    Serial.println(gostTime);
+    Serial.print("Old Speed: ");
+    Serial.println(oldSpeeds[plan]);
+    Serial.print("New Speed: ");
+    Serial.println(Motor[plan].control.maxSpeed);
   }
 }
+
+
+// long LinAccelDeaccel(STEPPER_MOTOR Motor, long steps, long step) {
+//   double maxSpeed = Motor.control.maxSpeed;
+//   double slope = Motor.control.AccelPFP;
+//   double degStep = Motor.control.degStep;
+//   long delta = (long)(maxSpeed / slope);
+//   long accelOffset = slope * steps;
+//   long alfa = steps / 2;
+//   double CurrentSpeed = 0;
+//   long plulseDealay = 0;
+
+//   if (steps > (2 * delta)) {
+//     if ((0 <= step) && (step <= delta)) {
+//       CurrentSpeed = slope * step;
+//     } else if ((delta < step) && (step <= (steps - delta))) {
+//       CurrentSpeed = maxSpeed;
+//     } else if (((steps - delta) < step) && (step <= steps)) {
+//       CurrentSpeed = -slope * step + accelOffset;
+//     } else if (steps < step) {
+//       CurrentSpeed = 0;
+//     }
+//   } else if (steps <= 2 * delta) {
+//     if ((0 <= step) && (step <= alfa)) {
+//       CurrentSpeed = slope * step;
+//     } else if (step > alfa) {
+//       CurrentSpeed = -slope * step + (2 * alfa);
+//     }
+//   }
+
+//   plulseDealay = (long)(((degStep * 60.0) / (CurrentSpeed * 360.0)) * pow(10, 6));
+
+//   if (plulseDealay > 50000) {
+//     plulseDealay = 10000;
+//   }
+
+//   return plulseDealay;
+// }
+
+//---------------------------------------------------------------------------------------------------------------------
+void singleLinRotate(STEPPER_MOTOR Motor, long steps) {
+
+  long plulseDealay = 0;
+  double maxSpeed = Motor.control.maxSpeed;
+  double degStep = Motor.control.degStep;
+  for (int step = 1; step <= steps; step++) {
+    plulseDealay = (long)(((degStep * 60.0) / (maxSpeed * 360.0)) * pow(10, 6));
+    singleStep(Motor, plulseDealay);
+  }
+}
+
+
 
 //---------------------------------------------------------------------------------------------------------------------
 void singleStep(STEPPER_MOTOR motor, long plulseDealay) {
@@ -57,6 +184,7 @@ void singleStep(STEPPER_MOTOR motor, long plulseDealay) {
   digitalWrite(motor.stepPin, LOW);
   delayMicroseconds(plulseDealay);
 }
+
 
 //---------------------------------------------------------------------------------------------------------------------
 void solveSystem(float A[][4], float b[], float x[], int size) {
--- Arduino code/fermwar_1/sketch_jun8a/sketch_jun8a.ino
+++ Arduino code/fermwar_1/sketch_jun8a/sketch_jun8a.ino
@@ -1,6 +1,5 @@
 //---------------------------- library prototypes ---------------------------------------------------------------
 #include <AccelStepper.h>
-#include <Arduino.h>
 #include <MultiStepper.h>
 #include <Servo.h>
 #include <Wire.h>
@@ -23,24 +22,20 @@ const double LMAX = L1 + L2 + L3 + L4;  // Maximum reach of the robot
 
 const int COMMAND_INDEX_NOT_FOUND = -1;  // Index value indicating command not found
 
-const double STEP_RESOLUTION = 1/32;                         // Possible resolutions for the driver
-const double STEP_PER_REV = (1000.0 / STEP_RESOLUTION);  // Steps per rotation
+const double RESOLUTIONS[15] = {400/400, 400/800 , 400/1600, 400/3200, 400/6400, 400/12800, 400/25600, 400/1000, 400/2000, 400/4000, 400/5000, 400/8000, 400/10000, 400/20000, 400,25000 }
+const double STEP_RESOLUTION = 1 / 32;                   // Possible resolutions for the driver
+const double STEP_PER_REV = (75000 / STEP_RESOLUTION);  // Steps per rotation
 const double DEG_PER_STEP = 360.0 / STEP_PER_REV;        // Degrees per step
 
-const long baceHomeAngle = 0;                    // Home angle for the shoulder joint
-const long shoulderHomeAngle = 0;                // Home angle for the shoulder joint
-const long elbowHomeAngle = 0;                   // Home angle for the shoulder joint
-const long elbowRevolutHomeAngle = 0;            // Home angle for the shoulder joint
-const long wristHomeAngle = 0;                   // Home angle for the shoulder joint
-const long wristRevolutHomeAngle = 0;            // Home angle for the shoulder joint
+const long baceHomeAngle = 0;          // Home angle for the bace joint
+const long shoulderHomeAngle = 0;      // Home angle for the shoulder joint
+const long elbowHomeAngle = 0;         // Home angle for the elbow joint
+const long elbowRevolutHomeAngle = 0;  // Home angle for the elbowRevolut joint
+const long wristHomeAngle = 0;         // Home angle for the wrist joint
+const long wristRevolutHomeAngle = 0;  // Home angle for the wristRevolut joint
 
 const char* seps = "\t,\n ;:";  // Delimiters for tokenizing the line string
 const double ERROR_VALUE = -1;  // Error value
-
-// number of points on path for every 500 units of arc length
-const int LOW_RESOLUTION_POINTS_PER_10_UNITS = 5;
-const int MEDIUM_RESOLUTION_POINTS_PER_10_UNITS = 10;
-const int HIGH_RESOLUTION_POINTS_PER_10_UNITS = 20;
 
 #define COMMAND_STRING_ARRAY_SIZE 502  // size of array to store commands written by sprintf_s for robot. 
                                        // NOTE: 2 elements must be reserved for trailing '\n' and '\0'
@@ -56,19 +51,16 @@ enum MOTOR_SPEED { MOTOR_SPEED_LOW,
 enum CURRENT_ANGLES { GET_CURRENT_ANGLES,
                       UPDATE_CURRENT_ANGLES };  // used to get/update current SCARA angles
 
-enum ARM { LEFT,
-           RIGHT };  // left arm or right arm configuration
-
 enum COMMAND_INDEX  // list of all command indexes
 {
-  ROTATE_JOINT, // Set all six angles
-  MOTOR_SPEED, // set the robot rotashion speed
-  GRIPPER_CLOSE, // open the gripper
-  GRIPPER_OPEN, // close the gripper
-  GRIPPER_OFFSET, // Set the Gripper offset
-  END, // turn the robot off
-  HOME, // move the robot to its home posishion
-  NUM_COMMANDS // The Number of commands
+  ROTATE_JOINT,    // Set all six angles
+  MOTOR_SPEED,     // set the robot rotashion speed
+  GRIPPER_CLOSE,   // close the gripper
+  GRIPPER_OPEN,    // open the gripper
+  GRIPPER_OFFSET,  // Set the Gripper offset
+  END,             // turn the robot off
+  HOME,            // move the robot to its home posishion
+  NUM_COMMANDS     // The Number of commands
 };
 
 //---------------------------- Program Global ----------------------------------------------------------------------
@@ -97,20 +89,9 @@ enum COMMAND_INDEX  // list of all command indexes
 #define wristRevolutStepPin 32
 #define wristRevolutDirPin 33
 
-Servo Gripper;                 // Gripper servo
-Servo GripperAngle;            // Gripper angle servo
+Servo Gripper;       // Gripper servo
+Servo GripperAngle;  // Gripper angle servo
 
-
-// structure to map command keyword string to a command index
-typedef struct STEPPER_MOTOR {
-  const int stepPin;
-  const int dirPin;
-  const long pulesRev;
-  const double current;
-  long speed;
-  double Acceleration;
-  const char* name;
-} STEPPER_MOTOR;
 
 // structure to map command keyword string to a command index
 typedef struct COMMAND {
@@ -119,77 +100,101 @@ typedef struct COMMAND {
 } COMMAND;
 
 // tooltip coordinates
-typedef struct TOOL_ORIENTATION {
-  double theta1Deg, theta2Deg; // the vector of hwo the Gripper is orientated
-} TOOL_ORIENTATION;
-
-// tooltip coordinates
-typedef struct TOOL_POSITION {
-  TOOL_ORIENTATION orientation;
-  double x, y, z; // the poshion of the grippers active area 
-} TOOL_POSITION;
-
-// joint angles (degrees)
-typedef struct REVOLUT_ANGLES {
-  double theta1Deg, theta2Deg; // the revolit angles of the sholder and wrist 
-} REVOLUT_ANGLES;
-
-// joint angles (degrees)
 typedef struct JOINT_ANGLES {
-  REVOLUT_ANGLES Revolut;
-  double theta1Deg, theta2Deg, theta3Deg, theta4Deg; // the angles of the bace, shoulder, elbow, wrist
+  double theta1Deg, theta2Deg, theta3Deg, theta4Deg, theta5Deg, theta6Deg;
 } JOINT_ANGLES;
 
-// forward kinematics solution data
-typedef struct FORWARD_SOLUTION {
-  TOOL_POSITION toolPos;  // tool tip coordinates
-  bool bCanReach;         // true if robot can reach, false if not
-} FORWARD_SOLUTION;
+// tooltip coordinates
+typedef struct TOOL_ORIENTATION {
+  double theta1Deg, theta2Deg;
+  double x, y, z;  // the poshion of the grippers active area
+} TOOL_ORIENTATION;
 
 // forward kinematics solution data
 typedef struct GRIPPER_STATE {
-  double angle;          // ratashion of gripper
-  bool actuste;         // true if gripper is close, false if open
+  double angle;  // ratashion of gripper
+  bool actuste;  // true if gripper is close, false if open
 } GRIPPER_STATE;
+
+// Setup the stepper motor
+MultiStepper steppersControl;
+
+AccelStepper bace(AccelStepper::FULL2WIRE, baceStepPin, baceDirPin);
+AccelStepper shoulder(AccelStepper::FULL2WIRE, shoulderStepPin, shoulderDirPin);
+AccelStepper elbow(AccelStepper::FULL2WIRE, elbowStepPin, elbowDirPin);
+AccelStepper elbowRevolut(AccelStepper::FULL2WIRE, elbowRevolutStepPin, elbowRevolutDirPin);
+AccelStepper wrist(AccelStepper::FULL2WIRE, wristStepPin, wristDirPin);
+AccelStepper wristRevolut(AccelStepper::FULL2WIRE, wristRevolutStepPin, wristRevolutDirPin);
+
+
 
 
 //----------------------------- Globals -------------------------------------------------------------------------------
 // global array of command keyword string to command index associations
-const COMMAND m_Commands[NUM_COMMANDS] = { { ROTATE_JOINT, "ROTATE_JOINT" }, { MOTOR_SPEED, "MOTOR_SPEED" }, 
-{ GRIPPER_CLOSE, "GRIPPER_CLOSE" }, { GRIPPER_OPEN, "GRIPPER_OPEN" }, { GRIPPER_OFFSET, "GRIPPER_OFFSET" }, { END, "END" }, { HOME, "HOME" } };
+const COMMAND m_Commands[NUM_COMMANDS] = { { ROTATE_JOINT, "ROTATE_JOINT" }, { MOTOR_SPEED, "MOTOR_SPEED" }, { GRIPPER_CLOSE, "GRIPPER_CLOSE" }, { GRIPPER_OPEN, "GRIPPER_OPEN" }, { GRIPPER_OFFSET, "GRIPPER_OFFSET" }, { END, "END" }, { HOME, "HOME" } };
 
 //----------------------------- Function Prototypes -------------------------------------------------------------------
 
-int nint(double); // Computes the nearest integer from a double value
-double degToRad(double); // Returns angle in radians from input angle in degrees
-double radToDeg(double); // Returns angle in degrees from input angle in radians
-double mapAngle(double); // Make sure inverseKinematic angles are mapped in range the robot understands
-void makeStringUpperCase(char*); // Makes an input string all uppercase
-void robotAngles(JOINT_ANGLES*, int); // Gets or updates the current SCARA angles
-FORWARD_SOLUTION forwardKinematics(JOINT_ANGLES); // Get the x,y,z position from the joint angles using forward kinematics
-bool setJointAngles(char*); // Set the angles of the joints based on the input command string
-bool setMotorSpeed(char*); // Set the motor speed based on the input command string
-bool setGripperState(char*); // Turn the gripper on or off based on the input command string
-bool setGripperRevolutAngle(char*); // Set the gripper angle based on the input command string
-void processCommand(int commandIndex, char*); // Processes a command string from the Serial input
-void runStepperAngles(JOINT_ANGLES); // Set the angles of the robot's joints and run the stepper motors accordingly
-void stepperMotorSetup(); // Setup everything for the stepper motors
-void ServoSetup(); // Setup the servo motors
-int getCommandIndex(char*); // Get the command keyword index from a string
+int nint(double);                                  // Computes the nearest integer from a double value
+double degToRad(double);                           // Returns angle in radians from input angle in degrees
+double radToDeg(double);                           // Returns angle in degrees from input angle in radians
+double mapAngle(double);                           // Make sure inverseKinematic angles are mapped in range the robot understands
+void makeStringUpperCase(char*);                   // Makes an input string all uppercase
+void robotAngles(JOINT_ANGLES*, int);              // Gets or updates the current SCARA angles
+bool setJointAngles(char*);                        // Set the angles of the joints based on the input command string
+bool setMotorSpeed(char*);                         // Set the motor speed based on the input command string
+bool setGripperState(char*);                       // Turn the gripper on or off based on the input command string
+bool setGripperRevolutAngle(char*);                // Set the gripper angle based on the input command string
+void processCommand(int commandIndex, char*);      // Processes a command string from the Serial input
+void runStepperAngles(JOINT_ANGLES);               // Set the angles of the robot's joints and run the stepper motors accordingly
+void stepperMotorSetup();                          // Setup everything for the stepper motors
+void ServoSetup();                                 // Setup the servo motors
+int getCommandIndex(char*);                        // Get the command keyword index from a string
 
 void setup() {
 
-  JOINT_ANGLES Home = { baceHomeAngle, shoulderHomeAngle, elbowHomeAngle,
-                      elbowRevolutHomeAngle, wristHomeAngle, wristRevolutHomeAngle };  // Define home angles for the robot
-  
-  robotAngles(&Home, UPDATE_CURRENT_ANGLES);                  // Update the robot's current angles to the home angles
-  stepperMotorSetup();                                        // Setup the stepper motor
-  ServoSetup();                                               // Setup the servo motor
+  // JOINT_ANGLES Home = { baceHomeAngle, shoulderHomeAngle, elbowHomeAngle,
+  //                       elbowRevolutHomeAngle, wristHomeAngle, wristRevolutHomeAngle };  // Define home angles for the robot
+
+  // robotAngles(&Home, UPDATE_CURRENT_ANGLES);  // Update the robot's current angles to the home angles
+  // ServoSetup();                               // Setup the servo motor
+
+  bace.setMaxSpeed(800);
+  shoulder.setMaxSpeed(800);
+  elbow.setMaxSpeed(800);
+  elbowRevolut.setMaxSpeed(800);
+  wrist.setMaxSpeed(800);
+  wristRevolut.setMaxSpeed(6000);
+
+  // add the each stepper motor to the controller
+  steppersControl.addStepper(bace);
+  steppersControl.addStepper(shoulder);
+  steppersControl.addStepper(elbow);
+  steppersControl.addStepper(elbowRevolut);
+  steppersControl.addStepper(wrist);
+  steppersControl.addStepper(wristRevolut);
 
   Serial.begin(9600);  // Start serial communication at 9600 baud
-  while (!Serial) {    // Wait for the serial port to connect
-    delay(100);
-  }
+
+  long positions[6] = { 0 };  // Array of desired stepper positions
+
+  positions[5] = 75000;
+
+
+  Serial.println("done");
+  steppersControl.moveTo(positions);
+  steppersControl.runSpeedToPosition();  // Blocks until all are in position
+  delay(1000);
+
+  // Move to a different coordinate
+  positions[5] = 0;
+
+
+  steppersControl.moveTo(positions);
+  steppersControl.runSpeedToPosition();  // Blocks until all are in position
+  delay(1000);
+
+  Serial.println("done");
 }
 
 char strLine[MAX_LINE_SIZE] = { '\0' };  // Define input buffer
@@ -198,28 +203,28 @@ char *token = NULL, *next_token = NULL;  // Token pointers for command processin
 
 
 void loop() {
-  if (Serial.available() > 0) {                         // If there is data available
-    char incoming = Serial.read();                      // Read the incoming character
-    if (incoming == '\n') {                             // If the incoming character is a newline
-      strLine[strnlen(strLine, MAX_LINE_SIZE)] = '\0';  // Terminate the input string
+  // if (Serial.available() > 0) {                         // If there is data available
+  //   char incoming = Serial.read();                      // Read the incoming character
+  //   if (incoming == '\n') {                             // If the incoming character is a newline
+  //     strLine[strnlen(strLine, MAX_LINE_SIZE)] = '\0';  // Terminate the input string
 
-      // Get the command index and process it
-      makeStringUpperCase(strLine);  // Make line string all uppercase (makes commands case-insensitive)
-      commandIndex = getCommandIndex(strLine);
-      token = strtok(strLine, seps);
-      token = strtok(NULL, "");  // Get the rest of the line as the next token
-      processCommand(commandIndex, token);
+  //     // Get the command index and process it
+  //     makeStringUpperCase(strLine);  // Make line string all uppercase (makes commands case-insensitive)
+  //     commandIndex = getCommandIndex(strLine);
+  //     token = strtok(strLine, seps);
+  //     token = strtok(NULL, "");  // Get the rest of the line as the next token
+  //     processCommand(commandIndex, token);
 
-      // Reset command variable to wait for the next command
-      memset(strLine, '\0', sizeof(strLine));                         // Clear the input buffer
-    } else {                                                          // If the incoming character is not a newline
-      if (strnlen(strLine, MAX_LINE_SIZE - 1) < MAX_LINE_SIZE - 2) {  // If there is room in the input buffer
-        strncat(strLine, &incoming, 1);                               // Add the incoming character to the input buffer
-      } else {                                                        // If the input buffer is full
-        Serial.println("Input buffer full, command ignored.");
-      }
-    }
-  }
+  //     // Reset command variable to wait for the next command
+  //     memset(strLine, '\0', sizeof(strLine));                         // Clear the input buffer
+  //   } else {                                                          // If the incoming character is not a newline
+  //     if (strnlen(strLine, MAX_LINE_SIZE - 1) < MAX_LINE_SIZE - 2) {  // If there is room in the input buffer
+  //       strncat(strLine, &incoming, 1);                               // Add the incoming character to the input buffer
+  //     } else {                                                        // If the input buffer is full
+  //       Serial.println("Input buffer full, command ignored.");
+  //     }
+  //   }
+  // }
 }
 
 
@@ -313,6 +318,10 @@ bool setJointAngles(char* strCommandLine) {
     if (token != NULL) {
       if (i == 0) angles.theta1Deg = (double)strtod(token, &token);  // set the first token to the theta1Deg
       if (i == 1) angles.theta2Deg = (double)strtod(token, &token);  // set the second token to the theta2Deg
+      if (i == 2) angles.theta3Deg = (double)strtod(token, &token);  // set the second token to the theta2Deg
+      if (i == 3) angles.theta4Deg = (double)strtod(token, &token);  // set the second token to the theta2Deg
+      if (i == 4) angles.theta5Deg = (double)strtod(token, &token);  // set the second token to the theta2Deg
+      if (i == 5) angles.theta6Deg = (double)strtod(token, &token);  // set the second token to the theta2Deg
 
       if (token[0] != '\0')  // theres was garbace traling the current string
       {
@@ -335,6 +344,8 @@ bool setJointAngles(char* strCommandLine) {
   }
   return goodCom;
 }
+
+
 
 //---------------------------------------------------------------------------------------------------------------------
 // DESCRIPTION:  will set the Gripper angle to the given angle
@@ -506,8 +517,8 @@ void makeStringUpperCase(char* str) {
 // RETURN VALUE: none
 void robotAngles(JOINT_ANGLES* pAngles, int getOrUpdate) {
   static JOINT_ANGLES currentAngles = { baceHomeAngle, shoulderHomeAngle, elbowHomeAngle,
-                                         elbowRevolutHomeAngle, wristHomeAngle, wristRevolutHomeAngle };  // NOTE:  robot must be in home position when program starts!
-  
+                                        elbowRevolutHomeAngle, wristHomeAngle, wristRevolutHomeAngle };  // NOTE:  robot must be in home position when program starts!
+
   if (pAngles == NULL)  // safety
   {
     Serial.println("NULL JOINT_ANGLES pointer! (robotAngles)");
@@ -522,45 +533,9 @@ void robotAngles(JOINT_ANGLES* pAngles, int getOrUpdate) {
     Serial.println("Unknown value for getOrUpdate (robotAngles)");
 }
 
-void stepperMotorSetup() {
-  Serial.begin(9600);  // Set up serial communication
-
-  pinMode(baceStepPin, OUTPUT);
-  pinMode(baceDirPin, OUTPUT);
-  
-  pinMode(shoulderStepPin, OUTPUT);
-  pinMode(shoulderDirPin, OUTPUT);
-
-  pinMode(elbowStepPin, OUTPUT);
-  pinMode(elbowDirPin, OUTPUT);
-
-  pinMode(elbowRevolutStepPin, OUTPUT);
-  pinMode(elbowRevolutDirPin, OUTPUT);
-
-  pinMode(wristStepPin, OUTPUT);
-  pinMode(wristDirPin, OUTPUT);
-
-  pinMode(wristRevolutStepPin, OUTPUT);
-  pinMode(wristRevolutDirPin, OUTPUT);
-
-  bace.setMaxSpeed(2000);
-  shoulder.setMaxSpeed(2000);
-  elbow.setMaxSpeed(2000);
-  elbowRevolut.setMaxSpeed(2000);
-  wrist.setMaxSpeed(2000);
-  wristRevolut.setMaxSpeed(2000);
-
-  steppersControl.addStepper(bace);  // add the each stepper motor to the controller
-  steppersControl.addStepper(shoulder);
-  steppersControl.addStepper(elbow);
-  steppersControl.addStepper(elbowRevolut);
-  steppersControl.addStepper(wrist);
-  steppersControl.addStepper(wristRevolut);
-}
 
 void ServoSetup() {
   //
-
 }
 
 size_t strnlen(const char* s, size_t len) {
