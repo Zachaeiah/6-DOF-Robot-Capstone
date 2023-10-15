38a1325b952b5ce757f6bc1608dcd0e549eb5699
Zachariah Loewen <zloewen314@gmail.com>
2023-10-14 21:39:05 -0700

    edited class motor and motorManager acording to uml file

--- app/backend/python code/MotorManager.py
+++ app/backend/python code/MotorManager.py
@@ -1,99 +1,207 @@
 from Motors import *
+import json
 
 class motorManager:
-    def __init__(self, motors: motor):
+    def __init__(self, motors: dict):
         """
-        Initialize the MotorManager.
+        Initializes a motor manager with a dictionary of motors.
 
-        :param motors: A list of Motor objects.
+        Args:
+            motors (dict): Dictionary of all the motors.
+
+        Raises:
+            TypeError: If the 'motors' argument is not a dictionary.
         """
+        if not isinstance(motors, dict):
+            raise TypeError("'motors' argument must be a dictionary.")
         self.motors = motors
-        # Create a dictionary mapping motor names to their instances for quick lookup
-        self.motor_name_to_instance = {motor.name: motor for motor in motors}
 
-        for i in range(len(self.motors), 1, 1):
-            link = (self.motors[i], self.motors[i-1])
-            self.linked_order.append(link)
-
-    def activate_motor(self, motor_name: str):
+    def activate(self, motor_name: str, state: bool) -> bool:
         """
-        Activate a motor by its name.
+        Activates or deactivates a motor.
 
-        :param motor_name: The name of the motor to activate.
+        Args:
+            motor_name (str): Name of the motor to activate or deactivate.
+            state (bool): True if the motor is to be activated, False if the motor is to be deactivated.
+
+        Returns:
+            bool: False if the motor doesn't exist, True otherwise.
         """
-        motor = self.motor_name_to_instance.get(motor_name)
-        if motor:
-            if not motor.is_activate:
-                motor.activate(True)
-                print(f"Motor '{motor_name}' is now active.")
+        try:
+            if motor_name in self.motors:
+                self.motors[motor_name].activate(state)
+                return True
             else:
-                print(f"Motor '{motor_name}' is already active.")
-        else:
-            print(f"Motor '{motor_name}' not found.")
+                print(f"Error: Motor with name '{motor_name}' not found.")
+                return False
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+            return False
 
-    def deactivate_motor(self, motor_name: str):
+    def remove_motor(self, motor_name: str) -> bool:
         """
-        Deactivate a motor by its name.
+        Removes a motor from the manager.
 
-        :param motor_name: The name of the motor to deactivate.
+        Args:
+            motor_name (str): Name of the motor to remove.
+
+        Returns:
+            bool: True if the motor was removed, False if the motor doesn't exist.
         """
-        motor = self.motor_name_to_instance.get(motor_name)
-        if motor:
-            if motor.is_activate:
-                motor.activate(False)
-                print(f"Motor '{motor_name}' is now deactivated.")
+        try:
+            if motor_name in self.motors:
+                del self.motors[motor_name]
+                return True
             else:
-                print(f"Motor '{motor_name}' is already deactivated.")
-        else:
-            print(f"Motor '{motor_name}' not found.")
+                print(f"Error: Motor with name '{motor_name}' does not exist.")
+                return False
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+            return False
 
-    def remove_motor(self, motor_name: str):
+    def get_motor_by_index(self, index: int):
         """
-        Remove a motor by its name.
+        Returns the motor with the specified index.
 
-        :param motor_name: The name of the motor to remove.
+        Args:
+            index (int): Index of the motor.
+
+        Returns:
+            motor: The motor with the specified index, or None if not found.
         """
-        motor = self.motor_name_to_instance.get(motor_name)
-        if motor:
-            self.motors.remove(motor)
-            del self.motor_name_to_instance[motor_name]
-            print(f"Motor '{motor_name}' removed.")
-        else:
-            print(f"Motor '{motor_name}' not found.")
-        
-        print()
-    def __repr__(self):
+        try:
+            for motor in self.motors.values():
+                if motor.index == index:
+                    return motor
+            print(f"Error: Motor with index '{index}' not found.")
+            return None
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+            return None
+
+    def get_motor_by_name(self, name: str):
         """
-        Return a string representation of the MotorManager that can be used to recreate it.
+        Returns the motor with the specified name.
+
+        Args:
+            name (str): Name of the motor.
+
+        Returns:
+            motor: The motor with the specified name, or None if not found.
         """
-        motor_list = [f"Motor(name='{motor.name}', is_activate={motor.is_activate}" for motor in self.motors]
-        return f"motorManager(motors=[{', '.join(motor_list)}])"
+        try:
+            return self.motors[name]
+        except KeyError:
+            print(f"Error: Motor with name '{name}' not found.")
+            return None
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+            return None
+
+    def write_motor_config(self, file_path: str) -> bool:
+        """
+        Writes the motor configuration to a JSON file.
+
+        Args:
+            file_path (str): Path to the JSON file.
+
+        Returns:
+            bool: True if writing is successful, False otherwise.
+        """
+        try:
+            with open(file_path, 'w') as json_file:
+                json.dump(self.motors, json_file, indent=4, default=lambda x: x.__dict__)
+            return True
+        except FileNotFoundError as e:
+            print(f"Error: File not found. {e}")
+            return False
+        except PermissionError as e:
+            print(f"Error: Permission denied. {e}")
+            return False
+        except TypeError as e:
+            print(f"Error: Type mismatch. {e}")
+            return False
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+            return False
+
+    def read_motor_config(self, file_path: str) -> bool:
+        """
+        Reads the motor configuration from a JSON file.
+
+        Args:
+            file_path (str): Path to the JSON file.
+
+        Returns:
+            bool: True if reading is successful, False otherwise.
+        """
+        try:
+            with open(file_path, 'r') as json_file:
+                data = json.load(json_file)
+                for motor_name, motor_data in data.items():
+                    if "type" in motor_data and motor_data["type"] == "Stepper Motor":
+                        new_motor = StepperMotor(
+                            motor_data.get("name"),
+                            motor_data.get("index"),
+                            motor_data.get("max_speed"),
+                            motor_data.get("max_acceleration"),
+                            motor_data.get("max_torqu"),
+                            motor_data.get("steps_per_revolution"),
+                        )
+                        if "is_activate" in motor_data and motor_data["is_activate"]:
+                            new_motor.activate(True)
+                        self.motors[motor_name] = new_motor
+                    else:
+                        raise ValueError("Motor type missing or not recognized.")
+            return True
+        except FileNotFoundError as e:
+            print(f"File not found error: {e}")
+            return False
+        except json.JSONDecodeError as e:
+            print(f"JSON decoding error: {e}")
+            return False
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return False
 
     def __str__(self):
         """
-        Return a human-readable string representation of the MotorManager.
-        """
-        motor_list = [f"Motor '{motor.name}' {'active' if motor.is_activate else 'inactive'}" for motor in self.motors]
-        return '\n'.join(motor_list)
-    
-    def __iter__(self):
-        # Allow iteration over the MotorManager, which will iterate over its Motor instances
-        return iter(self.motors)
+        Returns a string containing information about all motors in the manager.
 
+        Returns:
+            str: String containing motor information.
+        """
+        result = ""
+        for motor_name, motor in self.motors.items():
+            result += f"Motor Name: {motor.name}: {motor.is_activate}\n"
+
+        return result
 
 def main():
-    test_motors = []
-    for i in range(0, 6, 1):
-        test_motors.append(StepperMotor(f"Motor: {i}", 100, 10, 7e3, 27))
-
-        
+    test_motors = {f"Motor: {i}": StepperMotor(f"Motor: {i}", i, 100, 10, 1, 70_000) for i in range(0, 6, 1)}
     manager = motorManager(test_motors)
 
-    # manager.activate_motor("Motor: 3")
+    # Activate a motor
+    print(manager.activate("Motor: 2", True))  # Should return True
 
+    # Print the motors' information
     print(manager)
 
-    
+    # Remove a motor
+    print(manager.remove_motor("Motor: 3"))  # Should return True
+
+    # Write motor configurations to a JSON file
+    file_path = "motors_config.json"
+    manager.write_motor_config(file_path)
+
+    # Clear the motors data
+    manager.motors = {}
+
+    # Read motor configurations from the JSON file
+    manager.read_motor_config(file_path)
+
+    # Print the motors' information
+    print(manager)
 
 if __name__ == '__main__':
     main()
--- app/backend/python code/Motors.py
+++ app/backend/python code/Motors.py
@@ -1,29 +1,77 @@
 class motor:
     def __init__(self, name: str, index: int, max_speed: float, max_acceleration: float, max_torqu: float):
-        """_summary_
+        """
+        Initializes a motor with the provided parameters.
 
         Args:
-            name (str): Name of the motor
-            index (int): index of the motor
-            max_speed (float): top alowable speed 
-            max_acceleration (float): maximum acceleration
-            max_torqu (float): maximum torqu
+            name (str): Name of the motor.
+            index (int): Index of the motor.
+            max_speed (float): Maximum allowable speed.
+            max_acceleration (float): Maximum acceleration.
+            max_torqu (float): Maximum torque.
+
+        Raises:
+            ValueError: If any of the input parameters are invalid.
         """
-        
+        if not isinstance(name, str) or not name:
+            raise ValueError("Invalid name. Name must be a non-empty string.")
+        if not isinstance(index, int) or index < 0:
+            raise ValueError("Invalid index. Index must be a non-negative integer.")
+        if not isinstance(max_speed, (int, float)) or max_speed <= 0:
+            raise ValueError("Invalid max_speed. Max_speed must be a positive float.")
+        if not isinstance(max_acceleration, (int, float)) or max_acceleration <= 0:
+            raise ValueError("Invalid max_acceleration. Max_acceleration must be a positive float.")
+        if not isinstance(max_torqu, (int, float)) or max_torqu <= 0:
+            raise ValueError("Invalid max_torque. Max_torque must be a positive float.")
+
         self.name = name
         self.index = index
         self.max_speed = max_speed
         self.max_acceleration = max_acceleration
-        self.is_activate = False
         self.max_torqu = max_torqu
+        self.is_activate = False
+        self.type = None
 
-    def SetMaxSpeed(self, NewSpeed: float):
-        self.max_speed = NewSpeed
+    def set_max_speed(self, new_speed: float):
+        """
+        Set the maximum speed of the motor.
 
-    def setMaxAcceleration(self, NewAcceleration):
-        self.max_acceleration = NewAcceleration
+        Args:
+            new_speed (float): New maximum speed.
 
-    def activate(self, state = True):
+        Raises:
+                ValueError: If the new speed is not a positive float.
+        """
+        if not isinstance(new_speed, (int, float)) or new_speed <= 0:
+            raise ValueError("Invalid max_speed. Max_speed must be a positive float.")
+        self.max_speed = new_speed
+
+    def set_max_acceleration(self, new_acceleration: float):
+        """
+        Set the maximum acceleration of the motor.
+
+        Args:
+            new_acceleration (float): New maximum acceleration.
+
+        Raises:
+            ValueError: If the new acceleration is not a positive float.
+        """
+        if not isinstance(new_acceleration, (int, float)) or new_acceleration <= 0:
+            raise ValueError("Invalid max_acceleration. Max_acceleration must be a positive float.")
+        self.max_acceleration = new_acceleration
+
+    def activate(self, state: bool = True):
+        """
+        Activate or deactivate the motor.
+
+        Args:
+            state (bool, optional): True to activate, False to deactivate. Defaults to True.
+
+        Raises:
+            ValueError: If the state is not a boolean value.
+        """
+        if not isinstance(state, bool):
+            raise ValueError("Invalid state. State must be a boolean value.")
         self.is_activate = state
 
     def __str__(self) -> str:
@@ -36,28 +84,84 @@ class motor:
         return (
             f"Motor Information:\n"
             f"  Name: {self.name}\n"
-            f"  index: {self.index}\n"
+            f"  Index: {self.index}\n"
             f"  Max Speed: {self.max_speed} RPM\n"
             f"  Max Acceleration: {self.max_acceleration} RPM/s\n"
-            f"  Max torqu: {self.max_torqu}\n"
-            f"  Activate state: {self.is_activate}\n"
+            f"  Max Torqu: {self.max_torqu}\n"
+            f"  Activation State: {'Active' if self.is_activate else 'Inactive'}\n"
         )
     
 class StepperMotor(motor):
-    def __init__(self, name: str, index: int, max_speed: float, max_acceleration: float, max_torqu: float, steps_per_revolution: int):
+    def __init__(self, name: str, index: int, max_speed: float, max_acceleration: float, max_torque: float, steps_per_revolution: int):
+        """
+        Initialize a stepper motor with the provided parameters.
+
+        Args:
+            name (str): Name of the motor.
+            index (int): Index of the motor.
+            max_speed (float): Maximum allowable speed.
+            max_acceleration (float): Maximum acceleration.
+            max_torque (float): Maximum torque.
+            steps_per_revolution (int): Steps per revolution.
+        """
+        if not isinstance(max_speed, (int, float)) or max_speed <= 0:
+            raise ValueError("Invalid max_speed. Max_speed must be a positive float.")
+
         # Call the constructor of the base class (motor)
-        super().__init__(name, index, max_speed,  max_acceleration, max_torqu)
+        super().__init__(name, index, max_speed, max_acceleration, max_torque)
+        self.type = "Stepper Motor"
         self.steps_per_revolution = steps_per_revolution
 
     def __str__(self) -> str:
-        # Call the __str__ method of the base class (motor)
+        """
+        Return a string representation of the stepper motor's information.
+
+        Returns:
+            str: A formatted string containing stepper motor information.
+        """
         motor_info = super().__str__()
         return f"{motor_info}  Steps per Revolution: {self.steps_per_revolution}"
 
-    
-def main():
-    test_motor = StepperMotor(f"Motor: 1", 1, 100, 10, 1, 70_000)
-    print(test_motor)
+def test_motor():
+    try:
+        # Test the motor class
+        print("Testing Motor Class:")
+        test_motor = motor("Test Motor", 1, 100.0, 10.0, 1.0)
+        print(test_motor)
 
-if __name__ == "__main__":
-    main()
+        # Test setting max speed
+        test_motor.set_max_speed(150.0)
+        print(f"Max Speed Updated: {test_motor.max_speed}")
+
+        # Test setting max acceleration
+        test_motor.set_max_acceleration(15.0)
+        print(f"Max Acceleration Updated: {test_motor.max_acceleration}")
+
+        # Test activating the motor
+        test_motor.activate()
+        print(f"Motor Activated: {test_motor.is_activate}")
+
+        print("\nTesting StepperMotor Class:")
+        # Test the StepperMotor class
+        test_stepper_motor = StepperMotor("Test Stepper Motor", 2, 200.0, 20.0, 2.0, 80000)
+        print(test_stepper_motor)
+
+        # Test setting max speed for StepperMotor
+        test_stepper_motor.set_max_speed(250.0)
+        print(f"Max Speed Updated: {test_stepper_motor.max_speed}")
+
+        # Test setting max acceleration for StepperMotor
+        test_stepper_motor.set_max_acceleration(25.0)
+        print(f"Max Acceleration Updated: {test_stepper_motor.max_acceleration}")
+
+        # Test activating the StepperMotor
+        test_stepper_motor.activate()
+        print(f"Stepper Motor Activated: {test_stepper_motor.is_activate}")
+
+    except ValueError as ve:
+        print(f"ValueError: {ve}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+if __name__ == '__main__':
+    test_motor()
